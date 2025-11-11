6ca75ca6984985f621894292846032f0c8089f49
公交也刺激 <753280878@qq.com>
2025-11-11 14:40:27 +0800

    2025年11月11日：项目重构

--- HouseDeedPavilion/src/main/java/COM/House/Deed/Pavilion/Exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package COM.House.Deed.Pavilion.Exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
